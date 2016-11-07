13813ca225c91f1095da044c5081ab74ca6067a0
Manuel Xarez <manuelxarez@ua.pt>
2016-11-07 18:43:26 +0000

    Depth, says FUCK if it lost score due to processing time :D

--- student.py
+++ student.py
@@ -7,6 +7,7 @@ from functools import reduce
 class StudentAgent(Snake):
     def __init__(self, body=[(0,0)] , direction=(1,0)):
         super().__init__(body,direction,name="HouseCleaning")
+        self.oldPoints = 0
 
     def update(self,points=None, mapsize=None, count=None):
             self.nOpponents = len(points) - 1
@@ -17,19 +18,23 @@ class StudentAgent(Snake):
                 self.opponentPoints = [ x[1] for x in points if x[0] != self.name ][0]
 
     def updateDirection(self,maze):
+        if self.oldPoints > self.points:
+            print("fuck")
+        self.oldPoints = self.points
         studentAgent = self.body
         opponentsAgents = [x for x in maze.playerpos if x not in self.body]
         obstacles = maze.obstacles
         foodpos = maze.foodpos
         mazedata = (studentAgent,opponentsAgents,obstacles,foodpos) #Search for food
         oppMazedata = (opponentsAgents,studentAgent,obstacles,foodpos)
-        if foodpos in [self.result(oppMazedata,x)[0][0] for x in self.valid_actions(oppMazedata)]:
+        if self.nOpponents > 0 and foodpos in [self.result(oppMazedata,x)[0][0] for x in self.valid_actions(oppMazedata)]:
             actions = self.valid_actions(mazedata)
             if actions != []:
                 self.direction = actions[0]
         else:
             finalnode = self.aStar(mazedata)
             self.direction = finalnode.getAction()
+            
 
     def valid_actions(self,mazedata):
             validDirections = []
@@ -51,8 +56,10 @@ class StudentAgent(Snake):
         return 10*min(abs(pos2[0]-pos1[0]), (self.mapsize[0])-1-abs(pos2[0]-pos1[0]))  +  min(abs(pos2[1]-pos1[1]), self.mapsize[1]-1-abs(pos2[1]-pos1[1]))
 
     def isGoal(self,mazedata):
-        oppMazedata = (mazedata[1],mazedata[0],mazedata[2],mazedata[3])
-        blocksWay = any([self.valid_actions(self.result(oppMazedata, x)) == [] for x in self.valid_actions(oppMazedata)])
+        blocksWay = False
+        if self.nOpponents > 0:
+            oppMazedata = (mazedata[1],mazedata[0],mazedata[2],mazedata[3])
+            blocksWay = any([self.valid_actions(self.result(oppMazedata, x)) == [] for x in self.valid_actions(oppMazedata)])
         return mazedata[0][0] == mazedata[3] or blocksWay
 
     def result(self,mazedata, action):
@@ -68,7 +75,9 @@ class StudentAgent(Snake):
         frontier = []
         heappush(frontier, node)
         explored = []
-        while True:
+        depth = 0
+        while depth < 40:
+            depth += 1
             if frontier == []:
                 return node
 
@@ -89,3 +98,4 @@ class StudentAgent(Snake):
                 elif [x for x in frontier if x == child and x.costG > child.costG] != []:
                     frontier.remove(child)
                     heappush(frontier,child)
+        return node
