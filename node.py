377f5a153c15fdd84a62a552d546420349dc9974
David Almeida <davidmoreiraalmeida@ua.pt>
2016-12-03 19:38:13 +0000

    new HiNode for High level search

--- node.py
+++ node.py
@@ -25,3 +25,28 @@ class Node:
             action = node.action
             node = node.parent
         return action
+
+
+class HiNode:
+    def __init__(self,place,costG,costH,parent):
+        self.place = place
+        self.costG = costG
+        self.costH = costH
+        self.costF = costG + costH
+        self.parent = parent
+        self.depth = 0 if parent == None else parent.depth + 1
+
+    def __eq__(self,other):
+        if other == None:
+            return None
+        return self.place == other.place
+
+    def __lt__(self,other):
+        return self.costF < other.costF
+
+    def getPlace(self):
+        if self.parent == None:
+            return self.place
+        elif self.parent.parent == None:
+            return self.place
+        return self.parent.getPlace()
