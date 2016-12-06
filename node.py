b45d97c7ebd94e105241e1a2dfc0bf50e484ee7d
David Almeida <davidmoreiraalmeida@ua.pt>
2016-12-06 14:47:28 +0000

    node

--- node.py
+++ node.py
@@ -1,7 +1,5 @@
-import copy
-
 class Node:
-    def __init__(self,maze,costG,costH,action,parent):
+    def __init__(self, maze, costG, costH, action, parent):
         self.maze = maze
         self.costG = costG
         self.costH = costH
@@ -10,43 +8,49 @@ class Node:
         self.parent = parent
         self.depth = 0 if parent == None else parent.depth + 1
 
-    def __eq__(self,other):
-        if other == None:
+    def __eq__(self, other):
+        if other is None:
             return None
         return self.maze == other.maze
 
-    def __lt__(self,other):
+    def __lt__(self, other):
         return self.costF < other.costF
 
     def getAction(self):
         node = self
         action = self.action
-        while node.parent != None:
+        while node.parent is not None:
             action = node.action
             node = node.parent
         return action
 
 
 class HiNode:
-    def __init__(self,place,costG,costH,parent):
-        self.place = place
+    def __init__(self, gateway, costG, costH, parent, square, mapsize):
+        self.gateway = gateway
         self.costG = costG
         self.costH = costH
         self.costF = costG + costH
         self.parent = parent
-        self.depth = 0 if parent == None else parent.depth + 1
+        self.square = square
+        self.mapsize = mapsize
+        self.depth = 0 if parent is None else parent.depth + 1
 
-    def __eq__(self,other):
-        if other == None:
+    def __eq__(self, other):
+        if other is None:
             return None
-        return self.place == other.place
+        return self.gateway == other.gateway
 
     def __lt__(self,other):
         return self.costF < other.costF
 
+    def get_gateway_result(self):
+        x = self.gateway
+        return ((x[0][0]+x[1][0]) % self.mapsize[0], (x[0][1]+x[1][1]) % self.mapsize[1])
+
     def getPlace(self):
-        if self.parent == None:
-            return self.place
-        elif self.parent.parent == None:
-            return self.place
+        if self.parent is None:
+            return self.get_gateway_result()
+        elif self.parent.parent is None:
+            return self.get_gateway_result()
         return self.parent.getPlace()
